6c868077e653aea402433531bcc0806daaedb1d0
NeuroWinter <alex@pikori.com>
2021-12-07 18:31:38 +1300

    DONE

---
+++ 2021/day-07/part_two.py
@@ -0,0 +1,26 @@
+from typing import Dict, List
+
+def load_data(in_file: str) -> List[int]:
+    lines = []
+    with open(in_file, 'r') as f:
+        lines = f.readlines()
+        lines = [int(char) for char in "".join(lines).split(',') ]
+    return lines
+
+
+def get_fuel_burn(*, n: int, data: List[int]) -> int:
+    total = 0
+    for crab in data:
+        for x in range(1, abs(n - crab)+1):
+            total += x
+    return total
+
+
+def main():
+    data = load_data('in.txt')
+    fuel_burn_list = []
+    for x in range(0, max(data)):
+        fuel_burn_list.append(get_fuel_burn(n=x, data=data))
+    print(min(fuel_burn_list))
+
+main()
